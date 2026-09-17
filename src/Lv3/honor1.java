74ee35a4ba48ff47cde4af0c23c94c72199eae51
myhyeoeo <dk9790@naver.com>
2026-09-18 08:52:36 +0900

    53:명예의전당

---
+++ src/Lv3/honor1.java
@@ -0,0 +1,28 @@
+package Lv3;
+
+import java.util.*;
+    class Solution {
+        public int[] solution(int k, int[] score) {
+
+            int[] answer = new int[score.length];
+
+
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < score.length; i++) {
+                arr.add(score[i]);
+
+
+                Collections.sort(arr);
+
+
+                if (arr.size() < k) {
+                    answer[i] = arr.get(0);
+                } else {
+                    answer[i] = arr.get(arr.size() - k);
+                }
+            }
+
+            return answer;
+        }
+    }
